c3d43e9ecad51df90ded5156be81b83c5256b9de
Andres Rodriguez <andresrodriguez64@yahoo.com>
2020-01-19 21:40:42 -0800

    Implemented a side navigation menu that comes out from the right side of the page.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+function openNav() {
+	document.getElementById('my-side-nav').style.width = '250px';
+}
+
+function closeNav() {
+	document.getElementById('my-side-nav').style.width = '0';
+}
+
+let isClicked = false;
 function menuToggle(x) {
 	x.classList.toggle('change');
+	if (isClicked) {
+		closeNav();
+		isClicked = false;
+	} else {
+		openNav();
+		isClicked = true;
+	}
 }
